9a567636b47029d3e8562d577324caee100bfa6d
shenqingqing@maxrocky.com <shenqingqing@maxrocky.com>
2018-05-28 10:48:35 +0800

    忘记密码

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,3 +3,4 @@
  */
 export const BASE_API = 'http://result.eolinker.com/yWKj3U86390d5207ac80b2bc92aed3e38d447955aba1c82?uri=';
 export const BASE_MOCK_API = 'http://mock.eolinker.com/yWKj3U86390d5207ac80b2bc92aed3e38d447955aba1c82?uri=';
+export const BASI_GETCODE = 'http://result.eolinker.com/yWKj3U86390d5207ac80b2bc92aed3e38d447955aba1c82?uri=';
--- src/utils/asyncComponent.jsx
+++ src/utils/asyncComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// http://developer.51cto.com/art/201707/544250.htm     路由异步加载
 
 export default function asyncComponent(importComponent) {
   class AsyncComponent extends Component {
