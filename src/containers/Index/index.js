e690781de2349b77ceb4c2783332fcc0f78708b3
T00006331 <t00006331@vipshop.com>
2018-05-29 12:54:48 +0800

    tabbar初始化

--- src/containers/Index/index.js
+++ src/containers/Index/index.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 // import { is, fromJS } from 'immutable';
 import { hot } from 'react-hot-loader';
 import CSSModules from 'react-css-modules';
-// import { Toast } from 'antd-mobile';
+import { TabBar } from 'antd-mobile';
 // import { BASE_API as API } from '@/utils/api';
 // import fetch from '@/utils/fetch';
 import { connect } from 'react-redux';
 import getUserData from '@/redux/user/action';
-import ComHeader from '@/components/Header';
+// import ComHeader from '@/components/Header';
 import styles from './index.scss';
 
 
@@ -16,13 +16,160 @@ import styles from './index.scss';
 class Index extends Component {
   constructor(props) {
     super(props);
-    this.state = { };
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+      fullScreen: false
+    };
   }
-
+  renderContent(pageText) {
+    return (
+      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+        <div style={{ paddingTop: 60 }}> Clicked {pageText} tab, show {pageText} information.</div>
+        <a
+          style={{
+display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9'
+}}
+          onClick={(e) => {
+            e.preventDefault();
+            this.setState({
+              hidden: !this.state.hidden,
+            });
+          }}
+        >
+          Click to show/hide tab-bar
+        </a>
+        <a
+          style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
+          onClick={(e) => {
+            e.preventDefault();
+            this.setState({
+              fullScreen: !this.state.fullScreen,
+            });
+          }}
+        >
+          Click to switch fullscreen
+        </a>
+      </div>
+    );
+  }
   render() {
     return (
-      <div>
-        <ComHeader history={this.props.history}>首页</ComHeader>
+      // <div>
+      //   <ComHeader history={this.props.history}>首页</ComHeader>
+      // </div>
+      <div style={this.state.fullScreen ? {
+ position: 'fixed', height: '100%', width: '100%', top: 0
+} : { height: 400 }}
+      >
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+          hidden={this.state.hidden}
+        >
+          <TabBar.Item
+            title="首页"
+            key="首页"
+            icon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat'
+}}
+            />
+            }
+            selectedIcon={<div style={{
+              width: '22px',
+              height: '22px',
+              background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat'
+}}
+            />
+            }
+            selected={this.state.selectedTab === 'blueTab'}
+            badge={1}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab',
+              });
+            }}
+            data-seed="logId"
+          >
+            {this.renderContent('首页')}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat'
+}}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat'
+}}
+              />
+            }
+            title="客户"
+            key="客户"
+            badge="哈"
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab',
+              });
+            }}
+            data-seed="logId1"
+          >
+            {this.renderContent('客户')}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat'
+}}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat'
+}}
+              />
+            }
+            title="学习"
+            key="学习"
+            dot
+            selected={this.state.selectedTab === 'greenTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'greenTab',
+              });
+            }}
+          >
+            {this.renderContent('学习')}
+          </TabBar.Item>
+          <TabBar.Item
+            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+            title="我的"
+            key="我的"
+            selected={this.state.selectedTab === 'yellowTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'yellowTab',
+              });
+            }}
+          >
+            {this.renderContent('我的')}
+          </TabBar.Item>
+        </TabBar>
       </div>
     );
   }
